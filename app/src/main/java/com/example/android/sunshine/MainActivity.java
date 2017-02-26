19d38a580e53ee5bd01f418c3d43bb3e6a72839f
DhruvGala <dhruv.g123@gmail.com>
2017-02-26 16:42:00 -0500

    changed FrameView and added ScrollView

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,12 +17,30 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+
+        String []weatherData = {
+                "Today, Feb 26 - Clear - 10°C / 13°C",
+                "Tomorrow, Feb 27 - Rainy - 5°C / 8°C",
+                "Tuesday, Feb 28 - Snow - -1°C / 0°C",
+                "Wednesday, Mar 1 - Sunny - 10°C / 13°C",
+                "Thursday, Mar 2 - Sunny/Clear - 15°C / 17°C",
+                "Friday, Mar 3 - Clear - 10°C / 13°C",
+                "Saturday, Mar 4 - Clear - 10°C / 13°C",
+        };
+
+        for( String weatherDay : weatherData){
+            mWeatherTextView.append(weatherDay + "\n\n\n");
+        }
     }
 }
